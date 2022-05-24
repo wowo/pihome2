a8ed40b47daec90a9cbfea04a7884a2311385ffa
Wojciech Sznapka <wojciech.sznapka@sts.pl>
2022-05-24 11:31:51 +0200

    app skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {SensorsComponent} from "./sensors/sensors.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sensors', component: SensorsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
